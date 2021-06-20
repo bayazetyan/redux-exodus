6226cd979d1faa27268db82fedf337b3444e4041
Artashes Bayazetyan <bayazetyan@gmail.com>
2021-06-20 21:03:46 +0400

    Deploy website - based on 9639cd004617a38d075569aea78f3a5a5db56512

---
+++ assets/js/30a24c52.ff27a5b3.js
@@ -0,0 +1,1 @@
+(self.webpackChunkredux_exodus=self.webpackChunkredux_exodus||[]).push([[453],{516:function(e){"use strict";e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/blog/tags/hello"}')}}]);
---
+++ assets/js/e16015ca.751c12bd.js
@@ -0,0 +1,1 @@
+(self.webpackChunkredux_exodus=self.webpackChunkredux_exodus||[]).push([[700],{947:function(a){"use strict";a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/blog/tags/hola"}')}}]);
